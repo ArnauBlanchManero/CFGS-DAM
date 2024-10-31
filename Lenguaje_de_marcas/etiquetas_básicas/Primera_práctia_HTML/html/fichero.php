035f67b690a6c4e26009e6e827db421f85858731
ArnnauBlanchManero <arnau.b.manero@gmail.com>
2024-10-31 12:02:56 +0100

    Organización de ficheros HTML

---
+++ Lenguaje_de_marcas/etiquetas_básicas/Primera_práctia_HTML/html/fichero.php
@@ -0,0 +1,10 @@
+<?php
+if ($nombre == "arnau" && $contra == "1234") {
+    echo "Bienvenido Arnau!";
+} else {
+    echo "Vete de aquí";
+}
+
+// Corregir la estructura del enlace
+echo "<a href='Practica1_HTML.html'>Vuelve atrás</a>";
+?>

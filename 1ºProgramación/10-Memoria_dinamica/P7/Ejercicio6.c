98c24dfe12e06c5d9c30f1a0cc36c83a7f06cda6
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-03-06 12:28:44 +0100

    Acabo los ejercicios de la práctica 7

--- 1ºProgramación/10-Memoria_dinamica/P7/Ejercicio6.c
+++ 1ºProgramación/10-Memoria_dinamica/P7/Ejercicio6.c
@@ -26,7 +26,7 @@ int main(){
 	printf("Introduce cuanto quieres el desplazamiento para el cifrado cesar: ");
 	scanf("%d",&cifrado);
 	for (int i = 0; i < lon_frase; i++){//En este bucle llamamos a la funcion cifrado para que dandole un caracter y cuantos caracteres quiere el autobus saltar
-		arr[i] = cifrado_cesar((char)*(arr+i), cifrado);	
+		arr[i] = cifrado_cesar(*(arr+i), cifrado);	
 	}
 
 	printf("La frase '%s' antes de cifrar\nLa frase '%s' después de cifrar\n", frase, arr);
@@ -37,13 +37,13 @@ int main(){
 char cifrado_cesar(char frase, int saltos){
 	int letras_saltar = 0;
 	letras_saltar = frase;//Igualamos cada caracter recibido para convertirlo en un entero que podamos encriptar
-	if(frase >= 65 && frase <= 90){//Comparamos el caracter dentro de la tabla ascii para saber si es una letra mayúscula
+	if(frase >= 'A' && frase <= 'Z'){//Comparamos el caracter dentro de la tabla ascii para saber si es una letra mayúscula
 		letras_saltar = (letras_saltar - 13 + saltos) % 26;//Mediante este calculo obtendremos la de letras que necesitamos saltar o retroceder para que se permitan saltos negativos o mayores a 26 en el cifrado.
-		letras_saltar = letras_saltar + 65;//Esta ultima suma es para obtener el digito necesario
+		letras_saltar = letras_saltar + 'A';//Esta ultima suma es para obtener el digito necesario
 
-	} else if(frase >= 97 && frase <= 122){//Comparamos el caracter dentro de la tabla ascii para saber si es una letra minúscula
+	} else if(frase >= 'a' && frase <= 'z'){//Comparamos el caracter dentro de la tabla ascii para saber si es una letra minúscula
 		letras_saltar = (letras_saltar - 19 + saltos) % 26;//Mediante este calculo obtendremos la de letras que necesitamos saltar o retroceder para que se permitan saltos negativos o mayores a 26 en el cifrado.
-		letras_saltar = letras_saltar + 97;//Esta ultima suma es para obtener el digito necesario
+		letras_saltar = letras_saltar + 'a';//Esta ultima suma es para obtener el digito necesario
 	}
 	frase = letras_saltar;
 	return frase;//Devolvemos la frase osea el digito que vamos a intercambiar

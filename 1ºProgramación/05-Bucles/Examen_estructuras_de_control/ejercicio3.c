6ae434fd26d5504c90d733610ada49c43964c60a
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-03-13 09:22:57 +0100

    Separo C de Java

--- 1ºProgramación/05-Bucles/Examen_estructuras_de_control/ejercicio3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-	int num;
-	scanf("%d", &num);
-	(num % 2 == 0) ? printf("El número %d es par.\n", num) : printf("El número %d es impar.\n", num);
-	return 0;
-}

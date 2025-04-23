362f85c7c1fc728b2088f4e870b957f9ef55af66
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-04-23 13:53:33 +0200

    Seguimos con eventos de javascript

--- Lenguaje_de_marcas/javascript/Eventos/eventos.js
+++ Lenguaje_de_marcas/javascript/Eventos/eventos.js
@@ -3,16 +3,28 @@ function aleatorio(number){
 }
 
 function cambiarFondo(){
-    console.log("funciona");
+    console.log("color cambiado");
     document.body.style.background=`rgb(${aleatorio(255)},${aleatorio(255)},${aleatorio(255)})`;
+}
+
+function escribeTexto(event){
+    event.preventDefault();
+    let texto = document.getElementById("escribe").value;
+    document.getElementById("texto") += texto;
 }
 
 function load(){
     let boton= document.querySelector("button");
-    let cuadroTexto = document.getElementsByName("texto").value;
+    let escribeTexto = document.getElementById("escribe");
+    let boton3=document.getElementById("boton3");
+    let zona=document.getElementById("zona");
+    let formulario=document.getElementById("Formulario");
     
     boton.addEventListener("mousedown", cambiarFondo, false);
-    cuadroTexto.addEventListener("keydown", )
+    boton3.addEventListener("click",(event)=>console.log(event));
+    escribeTexto.addEventListener("keydown",(event)=>(document.getElementById("textoSalida").textContent += `${event.key}`));
+    zona.addEventListener("mousemove",(event)=>(document.getElementById("coordenada").textContent=`Coordenadas: X=${event.clientX} y Y=${event.clientY}`));
+    formulario.addEventListener("submit",escribeTexto);
 }
 
 window.addEventListener("DOMContentLoaded", load, false);
